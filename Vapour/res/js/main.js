8e9f7d4f3db95d8cec989f6b4a5bf58fb87563a9
Ollie Terrance <ollie.terrance@live.co.uk>
2016-04-10 11:14:27 +0100

    Get achievement fetching working

---
+++ Vapour/res/js/main.js
@@ -0,0 +1,108 @@
+function sortAlpha(a, b) {
+    var aa = a.name.toLowerCase().replace(/[^a-z0-9]/g, "");
+    var bb = b.name.toLowerCase().replace(/[^a-z0-9]/g, "");
+    return aa > bb ? 1 : (aa < bb ? -1 : 0);
+}
+function getFriends(fn) {
+    $.ajax({
+        "url": "https://steamcommunity.com/my/friends",
+        "success": function(resp, xhr, status) {
+            var friends = [];
+            $(".friendBlock", resp).each(function(i, friend) {
+                friends.push({
+                    "id": $(friend).find("input.friendCheckbox").data("steamid"),
+                    "name": $(friend).text().trim().split("\n")[0],
+                    "url": $(friend).attr("href")
+                });
+            });
+            friends.sort(sortAlpha);
+            fn(friends);
+        }
+    });
+}
+function getGames(fn, user) {
+    $.ajax({
+        "url": "https://steamcommunity.com/" + (user ? "profiles/" + user : "my") + "/games?tab=all",
+        "success": function(resp, xhr, status) {
+            var games = []
+            $(JSON.parse(resp.match(/var rgGames = (.+);/)[1])).each(function(i, game) {
+                games.push({
+                    "id": game.appid,
+                    "name": game.name,
+                    "image": game.logo
+                });
+            });
+            games.sort(sortAlpha);
+            fn(games);
+        }
+    });
+}
+function getStats(fn, game, user) {
+    $.ajax({
+        "url": "https://steamcommunity.com/" + (user ? "profiles/" + user : "my") + "/stats/" + game + "/?tab=achievements",
+        "success": function(resp, xhr, status) {
+            var stats = [];
+            $(".achieveRow", resp).each(function(i, stat) {
+                if ($(stat).find(".achieveHiddenBox").length) return;
+                var statObj = {
+                    "name": $(stat).find("h3").text(),
+                    "image": $(stat).find("img").attr("src"),
+                    "desc": $(stat).find("h5").text()
+                };
+                if ($(stat).find(".achieveUnlockTime").length) {
+                    statObj.date = $(stat).find(".achieveUnlockTime").text().trim().substring(9).split(" @ ");
+                }
+                if ($(stat).find(".progressText").length) {
+                    var parts = $(stat).find(".progressText").text().trim().split(/[^0-9]+/);
+                    statObj.progress = parts.map(Number);
+                }
+                stats.push(statObj);
+            });
+            fn(stats);
+        }
+    });
+}
+$(document).ready(function() {
+    $("#nav-profile").click(function(e) {
+        if ($(this).parent().hasClass("active")) return;
+        $("nav li.active").removeClass("active");
+        $(this).parent().addClass("active");
+        $("#pages > div").hide();
+        $("#page-profile").show();
+    }).click();
+    $("#nav-friends").click(function(e) {
+        if ($(this).parent().hasClass("active")) return;
+        $("nav li.active").removeClass("active");
+        $(this).parent().addClass("active");
+        $("#pages > div").hide();
+        $("#page-friends").show();
+    });
+    $("#page-profile").append($("<i>").addClass("fa fa-refresh fa-spin")).append(" Loading games...");
+    getGames(function(games) {
+        $("#page-profile").empty();
+        var $list = $("<ul>");
+        $(games).each(function(i, game) {
+            var $gameElmt = $("<li>").text(game.name);
+            $list.append($gameElmt);
+            getStats(function(stats) {
+                var $statsList = $("<ul>").addClass("fa-ul");
+                $(stats).each(function(i, stat) {
+                    var icon = stat.date ? "check-square-o" : (stat.progress ? "spinner" : "circle-o");
+                    $statsList.append($("<li>").append($("<i>").addClass("fa fa-li fa-" + icon)).append(stat.name));
+                });
+                $gameElmt.append($statsList);
+            }, game.id);
+            return false;
+        });
+        $("#page-profile").empty().append($list);
+    });
+    $("#page-friends").append($("<i>").addClass("fa fa-refresh fa-spin")).append(" Loading friends...");
+    getFriends(function(friends) {
+        $("#page-friends").empty();
+        var $list = $("<div>").addClass("row");
+        $(friends).each(function(i, friend) {
+            $list.append($("<div>").addClass("col-lg-2 col-md-3 col-sm-4 col-xs-6").text(friend.name));
+        });
+        $("#page-friends").empty().append($list);
+    });
+});
